14bb3b0a43a164dd94779beb8ec2848881bce03f
Angela Lynn Smith <angiesmith1226@gmail.com>
2023-12-29 15:01:09 -0500

    reloaded imported project files

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-function displayTemperature(response) {
-	let temperatureElement = document.querySelector("#current-temperature");
-	let temperature = Math.round(response.data.temperature.current);
-	let cityElement = document.querySelector("#current-city");
+function refreshWeather(response) {
+	let temperatureElement = document.querySelector("#temperature");
+	let temperature = response.data.temperature.current;
+	let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+
 	cityElement.innerHTML = response.data.city;
-	temperatureElement.innerHTML = temperature;
-}
-
-function search(event) {
-	event.preventDefault();
-	let searchInputElement = document.querySelector("#search-input");
-	let city = searchInputElement.value;
-
-	let apiKey = "cc8ad09bc07492ceeb391dfbot84812f";
-	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-
-	axios.get(apiUrl).then(displayTemperature);
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}m/ph`;
+    temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" 
+    class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
 	let minutes = date.getMinutes();
 	let hours = date.getHours();
-	let day = date.getDay();
-
-	if (minutes < 10) {
-		minutes = `0${minutes}`;
-	}
-
-	if (hours < 10) {
-		hours = `0${hours}`;
-	}
-
 	let days = [
 		"Sunday",
 		"Monday",
@@ -39,14 +31,29 @@ function formatDate(date) {
 		"Friday",
 		"Saturday",
 	];
+	let day = days[date.getDay()];
 
-	let formattedDay = days[day];
-	return `${formattedDay} ${hours}:${minutes}`;
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	return `${day} ${hours}:${minutes}`;
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+function searchCity(city) {
+	let apiKey = "cc8ad09bc07492ceeb391dfbot84812f";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+	axios.get(apiUrl).then(refreshWeather);
+}
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
-currentDateELement.innerHTML = formatDate(currentDate);
+function handleSearchSubmit(event) {
+	event.preventDefault();
+	let searchInput = document.querySelector("#search-form-input");
+
+	searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Esparza");
