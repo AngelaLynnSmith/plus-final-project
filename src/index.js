c296aaef187fe9db5723e3c4584bd531765bdf9d
Angela Lynn Smith <angiesmith1226@gmail.com>
2023-12-29 17:47:15 -0500

    injected the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -51,9 +51,34 @@ function handleSearchSubmit(event) {
 	let searchInput = document.querySelector("#search-form-input");
 
 	searchCity(searchInput.value);
-}
+ }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function displayForecast()	{
+	let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+	let forecastHtml = "";
+
+	days.forEach(function (day) {	
+	forecastHtml = 
+	forecastHtml +
+	`
+    <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+} ) ;	
+
+	let forecastElement = document.querySelector("#forecast");
+	forecastElement.innerHTML = forecastHtml;
+}
+	let searchFormElement = document.querySelector("#search-form");
+	searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Madrid");
+displayForecast()
