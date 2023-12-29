54d138f71f6a2473bb33ff12b7859ab53e5f188c
Angela Lynn Smith <angiesmith1226@gmail.com>
2023-12-29 15:04:31 -0500

    changed search city to Madrid

--- src/index.js
+++ src/index.js
@@ -56,4 +56,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Esparza");
+searchCity("Madrid");
